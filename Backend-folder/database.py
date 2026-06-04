b0ca00ba3ceb9fd80666251b9fae62fbd854c4be
NARASIMHA131415 <narasimhareddy131415@gmail.com>
2026-06-04 10:48:24 +0530

    Update database.py

--- Backend-folder/database.py
+++ Backend-folder/database.py
@@ -7,10 +7,11 @@ load_dotenv()
 
 class Database:
     def __init__(self):
-        self.host = os.getenv('DB_HOST', 'localhost')
-        self.user = os.getenv('DB_USER', 'root')
-        self.password = os.getenv('DB_PASSWORD', '')
-        self.database = os.getenv('DB_NAME', 'exam_portal')
+        self.host = os.getenv('DB_HOST')
+        self.user = os.getenv('DB_USER')
+        self.password = os.getenv('DB_PASSWORD')
+        self.database = os.getenv('DB_NAME')
+        self.port = int(os.getenv('DB_PORT', 3306))
 
     def get_connection(self):
         try:
@@ -18,7 +19,8 @@ class Database:
                 host=self.host,
                 user=self.user,
                 password=self.password,
-                database=self.database
+                database=self.database,
+                port=self.port
             )
             return connection
         except Error as e:
